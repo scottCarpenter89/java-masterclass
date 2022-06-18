18e66b5928efdcaf6a529a82895b3a239602f87e
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-17 23:26:03 -0500

    learned about numerical datatypes

--- udemy/java-master-class/ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ udemy/java-master-class/ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -14,6 +14,23 @@ public class Main {
 
         int myMaxIntTest = 2_147_483_647;
 
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value: " + myMinByteValue);
+        System.out.println("Byte Minimum Value: " + myMaxByteValue);
 
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value: " + myMinShortValue);
+        System.out.println("Short Minimum Value: " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value: " + myMinLongValue);
+        System.out.println("Long Minimum Value: " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
     }
 }

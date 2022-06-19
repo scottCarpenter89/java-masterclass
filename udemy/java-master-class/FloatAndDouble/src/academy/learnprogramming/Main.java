4491fd66fcd38d661f8b121547ae019d8b10541a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-19 17:20:29 -0500

    reviewed the float and double datatypes

--- udemy/java-master-class/FloatAndDouble/src/academy/learnprogramming/Main.java
+++ udemy/java-master-class/FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -14,8 +14,19 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25F;
-        double myDoubleValue = 5.25D;
+        int myIntValue = 5 / 3;
+        // returns 1; which is not precise because int double not handle floating decimals
+        float myFloatValue = 5F / 3F;
+        // returns 1.6666666; which is more precise than int
+        double myDoubleValue = 5D / 3D;
+        // returns 1.6666666666666667; which is more precise than the float
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloat = " + myFloatValue);
+        System.out.println("MyDouble = " + myDoubleValue);
+
+        double poundsToKilograms = 0.45359237;
+        double myWeightInPounds = 210;
+        double myWeightInKilograms = myWeightInPounds * poundsToKilograms;
+        System.out.println("My weight in kilograms is = " + myWeightInKilograms);
     }
 }

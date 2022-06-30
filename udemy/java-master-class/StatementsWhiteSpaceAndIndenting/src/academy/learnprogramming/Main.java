cff2947257d33d711cb330e13a4dc556daef9d49
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-29 23:47:49 -0500

    finished StatementsWhiteSpaceAndIndenting

--- udemy/java-master-class/StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
+++ udemy/java-master-class/StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
@@ -3,6 +3,25 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // the entire line including the datatype and semicolon are statements
+	    int myVariable = 50;
+        // other statements examples
+        myVariable++;
+        myVariable--;
+        System.out.println("This whole line is a statement");
+
+        System.out.println("This " +
+                "is" +
+                "another" +
+                "statement" +
+                "but " +
+                "on " +
+                "multiple" +
+                "lines.");
+
+        // this is valid to java as two separate statements, but isn't very readable
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+
+        // white space and indentation make the code more readable
     }
 }

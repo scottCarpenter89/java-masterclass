384d482e381bf9e9aa71f0e20dace2ecfccc17c9
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-29 23:28:32 -0500

    finished KeywordsAndExpressions

--- udemy/java-master-class/KeywordsAndExpressions/src/academy/learnprogramming/Main.java
+++ udemy/java-master-class/KeywordsAndExpressions/src/academy/learnprogramming/Main.java
@@ -3,6 +3,16 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
+        // the expression is everything except the data type and the semicolon
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        // the expression is everything inside parenthesis
+        if (highScore == 50) {
+            // everything in the parenthesis is an expression as well
+            System.out.println("This is an expression");
+        }
+
 
     }
 }

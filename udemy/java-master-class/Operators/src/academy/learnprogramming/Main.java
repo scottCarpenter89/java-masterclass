f210113ceda2dcbb14485ab035d9afa6d8d2ab74
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-29 23:05:22 -0500

    finished operator coding challenge

--- udemy/java-master-class/Operators/src/academy/learnprogramming/Main.java
+++ udemy/java-master-class/Operators/src/academy/learnprogramming/Main.java
@@ -44,5 +44,32 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens.");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You've got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than the second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+
+        // Operator Challenge
+        double value1 = 20.00d;
+        double value2 = 80.00d;
+        double value1AndValue2Product = (value1 + value2) * 100.00d;
+        double remainder = value1AndValue2Product % 40.00d;
+        boolean isRemainder = remainder == 0;
+        System.out.println(isRemainder);
+        if (!isRemainder) {
+            System.out.println("Got some remainder: " + remainder);
+        }
+
     }
 }
